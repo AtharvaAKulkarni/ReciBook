0ebf5166d6dde4c752844b7e98778a34f1a3123b
AtharvaAKulkarni <111754321+AtharvaAKulkarni@users.noreply.github.com>
2025-02-16 20:21:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
-
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   if (req.method === "OPTIONS") {
     return res.sendStatus(200); // Handle CORS preflight requests
   }
