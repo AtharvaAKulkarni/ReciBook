0a1885c6b0556c4d66aad97eb814f59b5de0c0eb
AtharvaAKulkarni <111754321+AtharvaAKulkarni@users.noreply.github.com>
2025-02-17 21:24:28 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("In Connect db");
-    console.log("Showing link here: ");
-    console.log(process.env.MONGODB_URI);
+    console.log("MONGODB_URI: ", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
